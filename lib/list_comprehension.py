74ed50c1b7e4699688fbc077e3c49a31078a118d
frastoneme <oigoeme@gmail.com>
2024-05-27 11:49:14 +0300

    adds  solution

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
     return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
     return [sentence + '!' for sentence in sentence_list]
-    print(return_evens([0, 1, 3, 5, 7, 8, 9]))
-    print(make_exclamation(["I like computers", "I require coffee", "Live long and prosper"]))
+
+num_list = [0, 1, 3, 5, 7, 8, 9]
+sentence_list = ["I like computers", "I require coffee", "Live long and prosper"]
+
+# using the above dummy data for illustration
+print(return_evens(num_list))
+print(make_exclamation(sentence_list))
